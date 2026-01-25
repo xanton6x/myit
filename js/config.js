acba03cb84bcd18f47707494fe8c80ffb8f6f4a4
xanton6x <toha400@gmail.com>
2026-01-25 21:35:23 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,6 @@
+// ייבוא הפונקציה מה-CDN של גוגל
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAYg4yMD9x-P8Z_xQi4qSCKZbHOZNdyq5Q",
   authDomain: "myit-4ed82.firebaseapp.com",
@@ -7,5 +10,8 @@ const firebaseConfig = {
   appId: "1:895445734032:web:d631db04306b1a0c11be15"
 };
 
-
+// אתחול האפליקציה
 const app = initializeApp(firebaseConfig);
+
+// ייצוא הקונפיגורציה כדי ש-auth.js יוכל להשתמש בה
+export default firebaseConfig;
